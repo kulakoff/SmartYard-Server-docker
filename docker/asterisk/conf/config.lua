8ab9fcfbe92ae0d5a5ab6da00983cb2bcd59178e
Anton Kulakov <anton.kulakoff@ya.ru>
2023-04-08 23:02:30 +0300

    added example asterisk config for running in docker

---
+++ docker/asterisk/conf/config.lua
@@ -0,0 +1,8 @@
+realm = "rbt"
+dm_server = "http://172.28.0.2/asterisk/extensions"
+redis_server_host = "172.28.0.4"
+redis_server_port = 6379
+redis_server_auth = "redis_passwd"
+log.outfile = false
+trunk = "first"
+lang = "ru"
